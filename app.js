f455cd9b41d7361b40c4a672b182c7c292ef0280
JEBREEL MAJDALWIEH <jemajdalwieh18@cit.just.edu.jo>
2023-08-16 00:12:16 +0300

    map api code

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-
-//!with rss:
+//! with maps
 
 document.addEventListener('DOMContentLoaded', function () {
     const jobTitle = document.querySelector('#job-title');
     const btn = document.querySelectorAll('.btn');
+    const mapBtn = document.querySelector('.map');
     let index = 0;
     const jobs = [];
+    let map;
 
-   
     function showLoading() {
         jobTitle.innerHTML = 'Job Title (fetching...) <i class="fas fa-spinner fa-spin"></i>';
     }
 
-    function Job(title) {
+    function Job(title, country) {
         this.title = title;
+        this.country = country;
     }
 
-   
     async function fetchJobs() {
-        showLoading(); 
+        showLoading();
 
         const proxyUrl = 'http://127.0.0.1:8080/';
         const targetUrl = 'https://careers.moveoneinc.com/rss/all-rss.xml';
@@ -32,19 +32,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
             for (let item of items) {
                 const title = item.getElementsByTagName("title")[0].textContent;
-                createJob(title);
+                const country = item.getElementsByTagName("country")[0].textContent;
+                createJob(title, country);
             }
 
-           
             jobTitle.textContent = jobs[0].title;
         } else {
             console.error("Error fetching RSS feed:", response.status, response.statusText);
         }
     }
 
-    function createJob(title) {
-        let job = new Job(title);
+    function createJob(title, country) {
+        let job = new Job(title, country);
         jobs.push(job);
+    }
+
+    function initMap(country) {
+        const geocoder = new google.maps.Geocoder();
+        map = new google.maps.Map(document.getElementById('map'));
+
+        geocoder.geocode({ 'address': country }, function (results, status) {
+            if (status == 'OK') {
+                map.setCenter(results[0].geometry.location);
+                new google.maps.Marker({
+                    map: map,
+                    position: results[0].geometry.location
+                });
+            } else {
+                console.error('Geocode was not successful for the following reason: ' + status);
+            }
+        });
     }
 
     btn.forEach(function (button) {
@@ -70,60 +87,12 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    
+    mapBtn.addEventListener('click', function (e) {
+        e.preventDefault();
+        initMap(jobs[index].country);
+    });
+
     fetchJobs();
 });
 
 
-//!no rss 
-// document.addEventListener('DOMContentLoaded', function () { 
-//     const jobTitle = document.querySelector('#job-title');
-    
-
-//     const btn = document.querySelectorAll('.btn');
-//     let index = 0;
-//     const jobs = [];
-
-//     function Job(title) {
-//         this.title = title;
-       
-//     }
-
-//     function createJob(title) {
-//         let job = new Job(title);
-//         jobs.push(job);
-//     }
-
-//     createJob("On Call Destination Consultants- Saudi Arabia")
-// createJob("Immigration Coordinator - Riyadh, Saudi Arabia")
-// createJob("Pet Transport Coordinator")
-
-    
-//     jobTitle.textContent = jobs[index].title;
-    
-
-//     btn.forEach(function (button) {
-//         button.addEventListener('click', function (e) {
-//             e.preventDefault(); 
-
-//             if (e.currentTarget.classList.contains('prev')) { 
-//                 if (index === 0) {
-//                     index = jobs.length;
-//                 }
-//                 index--;
-//                 jobTitle.textContent = jobs[index].title;
-               
-//             }
-
-//             if (e.currentTarget.classList.contains('next')) { 
-//                 index++;
-//                 if (index === jobs.length) {
-//                     index = 0;
-//                 }
-
-//                 jobTitle.textContent = jobs[index].title;
-               
-//             }
-//         });
-//     });
-// });
